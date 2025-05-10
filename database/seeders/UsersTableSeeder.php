849f8b8a7f59d69d5810905126e1548170e6d351
MOMO <>
2025-05-10 21:46:22 +0900

    初期データの登録　完了

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -13,6 +16,34 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            [
+                'over_name'       => '山田',
+                'under_name'      => '一郎',
+                'over_name_kana'  => 'ヤマダ',
+                'under_name_kana' => 'イチロウ',
+                'mail_address'    => 'yamada1@gmail.com',
+                'sex'             => 1, // 1=男性、2=女性、3=その他
+                'birth_day'       => '1990-01-01',
+                'role'            => 1, // 1=教師(国語)、2=教師(数学)、3=教師(英語)、4=生徒
+                'password'        => Hash::make('ichiro1'),
+                'created_at'      => now(),
+                'updated_at'      => now(),
+            ],
+            [
+                'over_name'       => '佐藤',
+                'under_name'      => '一子',
+                'over_name_kana'  => 'サトウ',
+                'under_name_kana' => 'イチコ',
+                'mail_address'    => 'ichiko1@gmail.com',
+                'sex'             => 2,
+                'birth_day'       => '1992-05-05',
+                'role'            => 2,
+                'password'        => Hash::make('ichiko1'),
+                'created_at'      => now(),
+                'updated_at'      => now(),
+            ],
+        ]);
 
     }
 }
