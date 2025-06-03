109ca2e8df1385fe11b62825987b66ebfdb32811
MOMO <>
2025-06-03 20:17:30 +0900

    サブカテゴリーのバリデーション表示追加

--- resources/views/authenticated/bulletinboard/post_create.blade.php
+++ resources/views/authenticated/bulletinboard/post_create.blade.php
@@ -37,6 +37,9 @@
   <div class="w-25 ml-auto mr-auto">
     <div class="category_area mt-5 p-5">
       <div class="">
+        @if($errors->first('main_category_name'))
+          <span class="error_message">{{ $errors->first('main_category_name') }}</span>
+        @endif
         <p class="m-0">メインカテゴリー</p>
         <input type="text" class="w-100" name="main_category_name" form="mainCategoryRequest">
         <input type="submit" value="追加" class="w-100 btn btn-primary p-0" form="mainCategoryRequest">
@@ -44,6 +47,9 @@
       <form action="{{ route('main.category.create') }}" method="post" id="mainCategoryRequest">{{ csrf_field() }}</form>
       <!-- サブカテゴリー追加 -->
       <div class="">
+       @if($errors->first('sub_category'))
+          <span class="error_message">{{ $errors->first('sub_category') }}</span>
+        @endif
         <p class="m-0">サブカテゴリー</p>
           <select class="w-100 mb-2" name="main_category_id" form="subCategoryRequest">
             @foreach($main_categories as $main_category)
