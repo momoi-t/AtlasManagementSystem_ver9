39dd81771360503830a1c108afa985dfe3c3924a
MOMO <>
2025-06-22 19:20:07 +0900

    予約詳細ページにユーザー情報を表示する

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -1,17 +1,23 @@
 <x-sidebar>
 <div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
   <div class="w-50 m-auto h-75">
-    <p><span>日</span><span class="ml-3">部</span></p>
+    <p><span>{{ $date }}日</span><span class="ml-3">　{{ $part }}部</span></p>
     <div class="h-75 border">
-      <table class="">
+      <table class="text-center">
         <tr class="text-center">
           <th class="w-25">ID</th>
           <th class="w-25">名前</th>
+          <th class="w-25">場所</th>
         </tr>
         <tr class="text-center">
-          <td class="w-25"></td>
-          <td class="w-25"></td>
+          @foreach ($reservePersons as $reserve)
+            @foreach ($reserve->users as $user)
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->over_name }} {{ $user->under_name }}</td>
+                <td>リモート</td>
         </tr>
+            @endforeach
+          @endforeach
       </table>
     </div>
   </div>
